ca12368458aa68e2fc9d8c0d1e4f3ffea83c71de
Christine Heying <christine-heying@uiowa.edu>
2015-11-02 14:01:28 -0600

    Created UpdateQuery.java

---
+++ src/java/dbHelpers/UpdateQuery.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbHelpers;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Pokemon;
+
+public class UpdateQuery {
+
+    private Connection conn;
+
+    public UpdateQuery(){
+    
+    Properties props = new Properties(); //MWC
+       InputStream instr = getClass().getResourceAsStream("dbConn.properties");
+        try {
+            props.load(instr);
+        } catch (IOException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            instr.close();
+        } catch (IOException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+       
+       String driver = props.getProperty("driver.name");
+       String url = props.getProperty("server.name");
+       String username = props.getProperty("user.name");
+       String passwd = props.getProperty("user.password");
+        try {
+            Class.forName(driver);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        try {
+            conn = DriverManager.getConnection(url, username, passwd);
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+    public void doUpdate (Pokemon pokemon){
+        
+        try {
+            String query = "UPDATE pokemon SET pokemonName = ?, baseExp = ?, type = ?, species = ?, region = ? WHERE pokemonID = ?";
+            
+            PreparedStatement ps = conn.prepareStatement(query);
+            
+            ps.setString(1, pokemon.getPokemonName());
+            ps.setInt(2, pokemon.getBaseExp());
+            ps.setString(3, pokemon.getType());
+            ps.setString(4, pokemon.getSpecies());
+            ps.setString(5, pokemon.getRegion());
+            
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(UpdateQuery.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+}
